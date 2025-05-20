d1b0b57a89803a144f5d294624eca90cf093b16d
Nhom3 <Nhom3@example.com>
2025-05-20 11:43:47 +0700

    Init Flask CI/CD demo

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
 from app import app
 
-def test_home():
+def test_app():
     res = app.test_client().get('/')
     assert res.status_code == 200
     assert b'Hello CI/CD' in res.data
 
 def test_hello():
     res = app.test_client().get('/hello/Alice')
-    assert b'Hello, Alice!' in res.data
+    assert b'Hello, Day la nhom 3!' in res.data
 
 def test_sum():
     res = app.test_client().get('/sum/3/4')
